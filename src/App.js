5f792b6644994c434b55863a776be07d0d353553
Craig Cartmell <craigcartmell1@gmail.com>
2018-01-14 12:37:27 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="w-full h-full bg-blue-darkest text-white overflow-hidden" />
+    );
+  }
+}
+
+export default App;
