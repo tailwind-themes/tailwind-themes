ed5fc5d9f97ee5422f41f9b3cf9d788f6ab9bd78
Craig Cartmell <craigcartmell1@gmail.com>
2018-01-14 20:21:23 +0000

    Completed form

--- src/App.js
+++ src/App.js
@@ -4,10 +4,74 @@ import { mobile } from 'react-icons-kit/icomoon/mobile';
 import { ic_language } from 'react-icons-kit/md/ic_language';
 import { browser } from 'react-icons-kit/iconic/browser';
 import TailwindIcon from './components/TailwindIcon';
+
 import './App.css';
 import Feature from './components/Feature';
 
 class App extends Component {
+  state = {
+    email: '',
+    error: null,
+    submitting: false,
+    subscribed: false
+  };
+
+  handleSubscribe = e => {
+    e.preventDefault();
+
+    this.setState({ submitting: true });
+
+    fetch(
+      `https://tailwind-themes.us17.list-manage.com/subscribe/post?u=cceff8adba7dcdcb1ca3d6b57&id=8b5f1b47b9&EMAIL=${encodeURIComponent(
+        this.state.email
+      )}`,
+      {
+        credentials: 'include'
+      }
+    )
+      // .then(() => this.setState({subscribed: true}))
+      // .catch((e) => this.setState({error: e.message}))
+      .finally(() => this.setState({ submitting: false, subscribed: true }));
+  };
+
+  renderForm() {
+    const disabled = !this.state.email || this.state.submitting;
+
+    if (this.state.subscribed) {
+      return (
+        <div className="text-teal-light">
+          Thank you for subscribing to updates{' '}
+          <span className="font-bold">{this.state.email}</span>! We'll be in
+          touch shortly.
+        </div>
+      );
+    }
+
+    return (
+      <form method="POST" noValidate onSubmit={this.handleSubscribe}>
+        <input
+          autoFocus
+          className="block md:inline w-full md:w-1/3 shadow appearance-none border rounded py-2 px-3 mr-1 mb-1 text-grey-darker"
+          name="EMAIL"
+          id="email"
+          type="email"
+          onChange={e => this.setState({ email: e.target.value })}
+          placeholder="Enter your email to receive the latest news"
+          value={this.state.email}
+        />
+        <button
+          disabled={disabled}
+          type="submit"
+          className={`rounded-sm bg-teal hover:bg-teal-dark text-white text-bold p-2 ${
+            disabled ? 'opacity-50 cursor-not-allowed' : ''
+          }`}
+        >
+          Sign up
+        </button>
+      </form>
+    );
+  }
+
   render() {
     return (
       <div className="App flex flex-col justify-center content-center items-center w-full h-screen bg-teal text-white p-4 overflow-auto">
@@ -16,21 +80,7 @@ class App extends Component {
           High quality website templates supercharged by TailwindCSS.
         </h2>
 
-        <div className="block mb-8 w-full text-center">
-          <input
-            autoFocus
-            className="block md:inline w-full md:w-1/3 shadow appearance-none border rounded py-2 px-3 mr-1 mb-1 text-grey-darker"
-            id="email"
-            type="email"
-            placeholder="Enter your email to receive the latest news"
-          />
-          <button
-            type="submit"
-            className="rounded-sm bg-teal-lighter hover:bg-teal-dark text-teal-darker text-bold p-2"
-          >
-            Sign up
-          </button>
-        </div>
+        <div className="block mb-8 w-full text-center">{this.renderForm()}</div>
 
         <div className="w-full md:w-3/4 h-auto flex flex-wrap">
           <Feature
@@ -46,7 +96,7 @@ class App extends Component {
           />
 
           <Feature
-            description="Create a website which works flawlessly on Chrome, Firefox, Internet Explorer and more."
+            description="Create a website which works flawlessly on Chrome, Firefox, Safari, Internet Explorer and more."
             icon={<Icon icon={browser} size={40} />}
             title="Cross-Browser Support"
           />
